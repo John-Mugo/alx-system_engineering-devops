920c55adecc00aff89286771c0092bdf4a5f0a7b
john-mugo <johnmwangi13115@gmail.com>
2023-04-24 06:57:22 -0700

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,7 +3,7 @@ class ssh_config {
   file { 'alx-system_engineering-devops/0x0B-ssh/.ssh/config':
     ensure  => file,
     content => "# Puppet-managed file. Do not edit.\n
-    HostName 54.90.3.58
+    HostName 52.201.163.140
     User ubuntu
     IdentityFile ~/.ssh/school
     PasswordAuthentication no\n",
